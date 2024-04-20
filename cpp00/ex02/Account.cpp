4d9d5467fb805bfb22dbba38f3b25211ab48cb75
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-04-20 14:00:25 +0200

    added first idea of timestampt. Still need to figure out why vscode makes the std::mem_fun_ref red in the tests.cpp

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,16 +6,20 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 00:12:16 by mevangel          #+#    #+#             */
-/*   Updated: 2024/04/20 13:04:39 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/04/20 13:57:24 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
-
-//? The public part of the class first:
+#include <iostream>
+#include <iomanip>
+#include <ctime>
 
 //Constructor:
-Account::Account( int initial_deposit ) {}
+Account::Account( int initial_deposit ) {
+
+	this->_amount = initial_deposit; //?first assumption. not sure. i need to verify.
+}
 
 //Destructor:
 Account::~Account( void ) {}
@@ -29,28 +33,74 @@ int Account::_totalNbWithdrawals = 0;
 
 // So that then the getters can be called:
 
+// Returns the total number of "Account" Objects created:
 int	Account::getNbAccounts( void ) {
-	
+
 	return _nbAccounts;
 }
 
+// Returns the total amount of money across all Account objects.
 int	Account::getTotalAmount( void ) {
 
 	return _totalAmount;
 }
 
+// Returns the total number of deposits made across all Account objects.
 int	Account::getNbDeposits( void ) {
-	
+
 	return _totalNbDeposits;
 }
 
+// Returns the total number of withdrawals made across all Account objects.
 int	Account::getNbWithdrawals( void ) {
 
 	return _totalNbWithdrawals;
 }
 
 
-static void	displayAccountsInfos( void ) {
+// Displays information about all Account objects, such as the number 
+// of accounts, total amount, etc.
+void	Account::displayAccountsInfos( void ) {
 
 	
+}
+
+
+
+// Allows making a deposit into the account. Increases the 
+// account balance (amount) by the specified deposit amount.
+void	Account::makeDeposit( int deposit ) {
+	
+	this->_amount += deposit;
+}
+
+
+// Allows making a withdrawal from the account. Checks if the account has 
+// sufficient balance (amount) to cover the withdrawal. If yes, it subtracts 
+// the withdrawal amount and returns true, otherwise returns false.
+bool	Account::makeWithdrawal( int withdrawal ) {
+	
+	
+}
+
+// Returns the current balance (amount) of the account object.
+int		Account::checkAmount( void ) const {
+
+	return this->_amount;
+}
+
+// Displays the status of the account, including its index, balance, 
+//number of deposits, and number of withdrawals.
+void	Account::displayStatus( void ) const {
+
+	
+}
+
+void Account::_displayTimestamp() {
+
+	std::time_t now = std::time(nullptr);
+	std::tm *localTime = std::localtime(&now);
+
+	// Format the timestamp
+	std::cout << "[" << std::put_time(localTime, "%Y%m%d_%H%M%S") << "]: ";
 }
