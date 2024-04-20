c13076621c675c68adeddab7daebf4c97375ca80
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-04-21 00:22:18 +0200

    cleaned the Account.cpp of internal comments, which I added to main comment above each function. diff gives correct result. Both in mac and linux.

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 00:12:16 by mevangel          #+#    #+#             */
-/*   Updated: 2024/04/20 19:21:14 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/04/21 00:09:30 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,146 +15,153 @@
 #include <iomanip>
 #include <ctime>
 
-// First, definition and initialization of the private static member variables, 
-// which are shared among all different objects of the class:
+// Initialization of the private static member variables, which are shared 
+// among all different objects of the class:
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-// So that then the getters can be called:
-
-// Getter for the total number of "Account" Objects created:
+// Getter for the total number of "Account" Objects created
 int	Account::getNbAccounts( void ) {
 
 	return _nbAccounts;
 }
 
-// Getter for the total amount of money across all Account objects.
+// Getter for the total amount of money across all Account objects
 int	Account::getTotalAmount( void ) {
 
 	return _totalAmount;
 }
 
-// Getter for the total number of deposits made across all Account objects.
+// Getter for the total number of deposits made across all Account objects
 int	Account::getNbDeposits( void ) {
 
 	return _totalNbDeposits;
 }
 
-// Getter for the total number of withdrawals made across all Account objects.
+// Getter for the total number of withdrawals made across all Account objects
 int	Account::getNbWithdrawals( void ) {
 
 	return _totalNbWithdrawals;
 }
 
-// The Private Constructor that can only be used from inside the class:
-Account::Account( void ) {}
-
-// The Public constructor that's called first, and needs an int argument:
-Account::Account( int initial_deposit ) {
-
-	// First we initialize the 4 attributes of the current "Account" object:
-	this->_amount = initial_deposit;
-	this->_accountIndex = _nbAccounts;
-	this->_nbDeposits = 0;
-	this->_nbWithdrawals = 0;
-
-	// and then we call the second, private constructor to print the message that
-	// the account was created, with its corresponding index and initial amount.
-	_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
-	
-	// and then we modify accordingly the static, shared among the objects, variables:
-	_nbAccounts++; // we increase it to be used in the construction of the next object:
-	_totalAmount += this->_amount; //we add this object's "amount" to the general shared amount of all accounts
-}
-
-//Destructor:
-Account::~Account( void ) {
-
-	// message example: [19920104_091532] index:0;amount:47;closed
-	_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
-}
-
-
-// Displays information about all Account objects, such as the number 
-// of accounts, total amount, etc.
-void	Account::displayAccountsInfos( void ) {
-
-	// message example: [19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
-	_displayTimestamp();
-	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
-}
-
-
-
-// Allows making a deposit into the account. Increases the 
-// account balance (amount) by the specified deposit amount.
-void	Account::makeDeposit( int deposit ) {
-	
-	// message example:: 
-	// [19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
-	
-	// So first we print the half message, with the initial amount:
-	_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";deposit:" << deposit;
-	
-	// Then we update the relative attributes of this object, according to the Deposit being made:
-	this->_amount += deposit;
-	this->_nbDeposits++;
-	// And then we update the relative static (shared) variables:
-	_totalAmount += deposit;
-	_totalNbDeposits++;
-
-	// And then we print the rest of the message: ;amount:47;nb_deposits:1
-	std::cout << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits << std::endl;
-}
-
-// Allows making a withdrawal from the account. Checks if the account has 
-// sufficient balance (amount) to cover the withdrawal. If yes, it subtracts 
-// the withdrawal amount and returns true, otherwise returns false.
-bool	Account::makeWithdrawal( int withdrawal ) {
-	
-	if (withdrawal >= this->_amount) {
-		// message example: [19920104_091532] index:0;p_amount:47;withdrawal:refused
-		_displayTimestamp();
-		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:refused" << std::endl;
-		return false;
-	}
-	else {
-		// message example: [19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
-		_displayTimestamp();
-		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:" << withdrawal;
-		
-		// Then we update the relative attributes of this object, according to the Deposit being made:
-		this->_amount -= withdrawal;
-		this->_nbWithdrawals++;
-		// And then we update the relative static (shared) variables:
-		_totalAmount -= withdrawal;
-		_totalNbWithdrawals++;
-
-		//and then we print the rest of the message: ;amount:785;nb_withdrawals:1
-		std::cout << ";amount:" << this->_amount << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
-		return true;
-	}
-}
-
-// Returns the current balance (amount) of the account object.
+// Getter for the amount of the current "Account" Object
 int		Account::checkAmount( void ) const {
 
 	return this->_amount;
 }
 
-// Displays the status of the current Account Object.
+// The Private Constructor that can only be used from inside the class
+Account::Account( void ) {}
+
+/*
+*	Public Constructor that initializes the 4 private attributes of the current Account object, 
+*	displays a message of syntax "[19920104_091532] index:0;amount:42;created" to show the 
+*	initialization of the current Account, and updates the static, general for the class, values
+*	"_nbAccounts" and "_totalAmount".
+*	@param initial_deposit: the amount that the current Account "opens with".
+*/
+Account::Account( int initial_deposit ) {
+
+	this->_amount = initial_deposit;
+	this->_accountIndex = _nbAccounts;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+
+	_nbAccounts++;
+	_totalAmount += this->_amount;
+}
+
+/*
+*	Destructor for the Account objects that also prints a message of syntax/pattern:
+*	"[19920104_091532] index:0;amount:47;closed", corresponding to the current Account object.
+*/
+Account::~Account( void ) {
+
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
+}
+
+/*
+*	Makes a deposit into the current "Account" object, modifying accordingly the object's and 
+*	class' variables that are related to deposit. Prints a message of type:
+*	[19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
+*	@param deposit: the amount to deposit to the current Account object.
+*/
+void	Account::makeDeposit( int deposit ) {
+	
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";deposit:" << deposit;
+
+	this->_amount += deposit;
+	this->_nbDeposits++;
+
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+
+	std::cout << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits << std::endl;
+}
+
+/*
+*	Makes a withdrawal from the current "Account" object, modifying accordingly the object's and 
+*	class' variables that are related to withdrawal. Prints a message of type:
+*	"[19920104_091532] index:0;p_amount:47;withdrawal:refused" if the existing amount is not enough
+*	to withdraw, or "[19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1",
+*	if the existing amount is enough to withdraw.
+*	@param withdrawal: the amount to withdraw from the current Account object.
+*	@returns true, if the asked withdraw was possible, or
+*			 false, if there was not enough amount to process the asked withdraw.
+*/
+bool	Account::makeWithdrawal( int withdrawal ) {
+	
+	if (withdrawal >= this->_amount) {
+		_displayTimestamp();
+		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:refused" << std::endl;
+		return false;
+	}
+	else {
+		_displayTimestamp();
+		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:" << withdrawal;
+
+		this->_amount -= withdrawal;
+		this->_nbWithdrawals++;
+
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals++;
+
+		std::cout << ";amount:" << this->_amount << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+		return true;
+	}
+}
+
+/*
+*	Displays the cumulative information of all the objects of the class "Account", printing a message
+*	of type: "[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0"
+*/
+void	Account::displayAccountsInfos( void ) {
+
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+/*
+*	Displays all the information of the current "Account" object, printing a message
+*	of type: "[19920104_091532] index:0;amount:42;deposits:0;withdrawals:0"
+*/
 void	Account::displayStatus( void ) const {
 
-	//message example: [19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
+/*
+*	Prints a timestamp of type: "[YEAR(4)MONTH(2)DAY(2)_HOUR(2)MINUTE(2)SECOND(2)] "
+*	Example: [20240421_150516]
+*/
 void Account::_displayTimestamp() {
 
 	std::time_t now = std::time(NULL);
