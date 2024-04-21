d22b9b7c416c3cc856b6da69c6beaff64150d682
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-04-22 01:41:32 +0200

    cleaned the comments from the main in 01ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 12:56:20 by mevangel          #+#    #+#             */
-/*   Updated: 2024/04/14 13:48:31 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/04/22 01:35:38 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,37 @@
 #include <fstream>
 
 #define ERROR "\033[31m" << "ERROR: " << "\033[0m"
+#define BOLD(text) "\033[1m" << text << "\033[0m"
 
-static std::string replace_s1_with_s2(char* argv[], std::string content) {
+static std::string replace_s1_with_s2(char **argv, std::string content) {
 
 	std::string s1 = argv[2]; //the str to be changed
-	if (s1.empty() == true) //if s1, there's nothing we want to replace
+	if (s1.empty() == true)
 		return content;
 
 	std::string s2 = argv[3]; //the new str, to change the occurences of s1 with
-	std::string ret; //the string that I will do the modifications on, and return with this function
+	std::string ret;
 
-	size_t s1_idx = content.find(s1, 0); // to find the first occurence of s1.
+	size_t s1_idx = content.find(s1, 0); // finds the first occurence of s1.
 	size_t start_idx = 0;
 	
 	//if the s1 is not found inside content, the find() will return the size_t npos
-	while (s1_idx != std::string::npos) { // =while the s1_idx is not equal with the index of the last character of the string
+	while (s1_idx != std::string::npos) { // :while s1_idx is not equal with the index of the last character of the string
 
 		ret += (content.substr(start_idx, s1_idx - start_idx) + s2);
 		start_idx = s1_idx + s1.length();
 		s1_idx = content.find(s1, start_idx);
 	}
-	ret += content.substr(start_idx); //to add the remainins of content after the last occurence of s1
+	ret += content.substr(start_idx); //to add the remainings of content after the last occurence of s1
 
-	return (ret);
+	return ret;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char **argv) {
 
 	// Error handling for wrong number of arguments:
 	if (argc != 4) {
-		std::cerr << ERROR << "Expected syntax: " << argv[0] << " filestream s1 s2" << std::endl;
+		std::cerr << ERROR << "Expected syntax: " << BOLD("./replace filestream s1 s2") << std::endl;
 		return 0;
 	}
 
@@ -68,7 +69,7 @@ int main(int argc, char* argv[]) {
 	}
 	in_stream.close();
 
-	//! Call the main replace function:
+	// the main replace function:
 	content = replace_s1_with_s2(argv, content);
 
 	// Copy the modified content to the output.file:
@@ -79,9 +80,9 @@ int main(int argc, char* argv[]) {
 		return 1; 
 	}
 	
-	// And now we write the "content" to the output_file and we close the ofstream:
+	// Write the "content" to the output_file and close the ofstream:
 	out_stream << content;
 	out_stream.close();
 
-	return (0);
+	return 0;
 }
