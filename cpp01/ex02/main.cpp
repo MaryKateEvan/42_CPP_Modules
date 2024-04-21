a6a444233c135ceb4317e192819e13cff83070a7
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-04-22 00:30:44 +0200

    added colors in main.cpp to make the comparisons more obvious

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,32 +6,36 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 12:56:20 by mevangel          #+#    #+#             */
-/*   Updated: 2024/04/12 13:16:37 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/04/22 00:27:09 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
+#define GREEN(text) "\033[32m" << text << "\033[0m"
+#define BLUE(text) "\033[34m" << text << "\033[0m"
+#define RED(text) "\033[31m" << text << "\033[0m"
+
 int main() {
 
 	//Initializations:
-	std::string str = "HI THIS IS BRAIN"; //the const string
+	std::string str = "HI THIS IS BRAIN";
 	std::string* stringPTR = &str;
 	std::string& stringREF = str;
 	std::cout << std::endl;
 
 	//Printing of the memory addresses:
-	std::cout << "Memory address of the string variable: " << &str << std::endl;
-	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
-	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << "Memory address of the " << GREEN("string variable") << ": " << &str << std::endl;
+	std::cout << "Memory address held by " << BLUE("stringPTR") << ": " << stringPTR << std::endl;
+	std::cout << "Memory address held by " << RED("stringREF") << ": " << &stringREF << std::endl;
 	std::cout << std::endl;
 	
 	//Printing of the values:
-	std::cout << "Value of the string variable: " << str << std::endl;
-	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
-	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+	std::cout << "Value of the " << GREEN("string variable") << ": " << str << std::endl;
+	std::cout << "Value pointed to by " << BLUE("stringPTR") << ": " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by " << RED("stringREF") << ": " << stringREF << std::endl;
 	std::cout << std::endl;
 	
-	return (0);
+	return 0;
 }
