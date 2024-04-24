67dd711fbaac8e54720432576c04462ae68c2f6c
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-04-24 02:22:08 +0200

    added the copy assignment operator with the corresponding getter

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,14 +6,14 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 01:42:31 by mevangel          #+#    #+#             */
-/*   Updated: 2024/04/24 01:51:04 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/04/24 02:11:53 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 // Default constructor
-Fixed::Fixed() {
+Fixed::Fixed() : _fpnv(0) {
 
 	std::cout << "Default constructor called" << std::endl;
 }
@@ -31,8 +31,29 @@ Fixed::~Fixed () {
 	std::cout << "Destructor called" << std::endl;
 }
 
-// Copy assignment Operator:
+// Getter for the private attribute _fpnv:
+int Fixed::getFpnv() const {
+
+	return this->_fpnv;
+}
+
+// Copy assignment Operator overload:
 Fixed & Fixed::operator=(Fixed const & src) {
+
+	std::cout << "Copy assignment operator called" << std::endl;
+	
+	if (this != &src)
+		this->_fpnv = src.getFpnv();
+	
+	return *this;
+}
+
+int Fixed::getRawBits(void) const {
+
+	
+}
+
+void Fixed::setRawBits(int const raw) {
 
 	
 }
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 01:40:01 by mevangel          #+#    #+#             */
-/*   Updated: 2024/04/24 01:50:24 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/04/24 02:17:18 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,20 @@ class Fixed {
 
 	private:
 
+		int _fpnv; //for Fixed-Point Number Value
+		static const int fract_bits = 8; //it means that i have 8 bits for the fractional part. So if the int is 32-bit, i would have 24 bits left for the integer part.
 
 	public:
 
-		Fixed();								// constructor
+		Fixed();								// default constructor
 		Fixed(Fixed const & to_copy);			// copy constructor
 		~Fixed();								// destructor
 		
-		Fixed & operator=(Fixed const & src);	// Copy assignment operator
+		int getFpnv() const;
+		Fixed & operator=(Fixed const & src);	// Copy assignment operator overload
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
 };
 
 
