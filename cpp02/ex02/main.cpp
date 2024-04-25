4eb56e6a5c5e95a59c962df526cc249d3fb0f302
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-04-25 05:58:49 +0200

    added multiple tests for all cases in the main of 02ex02.

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 04:02:47 by mevangel          #+#    #+#             */
-/*   Updated: 2024/04/25 05:24:38 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/04/25 05:57:46 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,9 @@
 #define IN_PURPLE(text) "\033[35m" << text << "\033[0m"
 #define IN_CYAN(text) "\033[36m" << text << "\033[0m"
 
-
 int main() {
 
 	// THE TEST FROM THE SUBJECT PDF:
-	// ...from here
 	{
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
@@ -42,25 +40,41 @@ int main() {
 	
 	std::cout << Fixed::max( a, b ) << std::endl;
 	}
-	// ...until here.
 
 	// ADDITIONAL TESTS:
 	{
 	Fixed a(10.5f); 		// for integer constructor
-	Fixed b(2);				// for float constructor
+	Fixed const b(2);		// for float constructor
 	Fixed c(a);				// for copy constructor
 	
-	Fixed sum(a + c);
-	Fixed dif(a - b);
-	Fixed mult(b * c);
-	Fixed div(a / c);
-	
 	std::cout << std::endl;
-	std::cout << UNDERLINE("Testing the arithmetic operations of the Fixed Objects:") << std::endl;
-	std::cout << "The " << BOLD("sum") << " of objects a and c is " << sum << std::endl;
-	std::cout << "The " << BOLD("difference") << " of object b from a is " << dif << std::endl;
-	std::cout << "The product of the " BOLD("multiplication") << " of object b and c is " << mult << std::endl;
-	std::cout << "The quotent of object a " << BOLD("divided") << " by c is " << div << std::endl;
+	std::cout << UNDERLINE("Arithmetic operations on Fixed Objects:") << std::endl;
+	std::cout << "a + b = " << (a + b) << std::endl;
+	std::cout << "a - b = " << (a - b) << std::endl;
+	std::cout << "a * b = " << (a * b) << std::endl;
+	std::cout << "a / b = " << (a / b) << std::endl;
+
+	std::cout << std::endl;
+	std::cout << UNDERLINE("Comparison Operations on Fixed Objects:") << std::endl;
+	std::cout << "a < b  is " << (a < b) << std::endl;
+	std::cout << "a > b  is " << (a > b) << std::endl;
+	std::cout << "a <= b is " << (a <= b) << std::endl;
+	std::cout << "a >= b is " << (a >= b) << std::endl;
+	std::cout << "a == b is " << (a == b) << std::endl;
+	std::cout << "a != b is " << (a != b) << std::endl;
+
+	std::cout << std::endl;
+	std::cout << UNDERLINE("Min/Max Overload Functions:") << std::endl;
+	std::cout << "a = " << a << " and b = " << b << std::endl;
+	std::cout << "The min from a and b is: " << Fixed::min(a, b) << std::endl;
+	std::cout << "And the max from a and b is: " << Fixed::max(a, b) << std::endl;
+
+	std::cout << std::endl;
+	std::cout << UNDERLINE("Increment/Decrement operations on Fixed Objects") << std::endl;
+	std::cout << "Object \"a\" initially is: " << a << std::endl;
+	std::cout << "after ++a it becomes: " << ++a << std::endl;
+	std::cout << "and after a++: " << a++ << std::endl;
+	std::cout << std::endl;
 	}
 
 	return 0;
