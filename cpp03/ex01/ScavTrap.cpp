498e8c7076257ccd225e8d764fea4332dd18d7cc
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-04-26 04:34:46 +0200

    first attempt in 03ex01. Kept only the canonical form elements in the scavtrap, changed the message in attack and printStatus functions, and refactored the constructors to call the constructors of ClapTrap initially

---
+++ cpp03/ex01/ScavTrap.cpp
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 21:39:07 by mevangel          #+#    #+#             */
+/*   Updated: 2024/04/26 04:22:25 by mevangel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+// Default constructor
+ScavTrap::ScavTrap() : ClapTrap() {
+
+	this->_Name = "unknown ScavTrap";
+	this->_HitPoints = 100;
+	this->_EnergyPoints = 50;
+	this->_AttackDamage = 20;
+	
+	std::cout << "Constructor for ScavTrap called" << std::endl;
+};
+
+// Parameter constructor to assign the name taken as parameter, and initialize the other three attributes.
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
+
+	this->_HitPoints = 100;
+	this->_EnergyPoints = 50;
+	this->_AttackDamage = 20;
+
+	std::cout << "Constructor for ScavTrap called" << std::endl;
+};
+
+// Additional Parameter Constructor, if the user wants to assign all attributes directly:
+ScavTrap::ScavTrap(std::string name, unsigned int hitPoints, unsigned int energyPoints, unsigned int attackdamage) 
+	: ClapTrap(name, hitPoints, energyPoints, attackdamage) {
+		
+	std::cout << "Constructor for ScavTrap called" << std::endl;
+};
+
+// Copy constructor
+ScavTrap::ScavTrap(ScavTrap const & to_copy) {
+
+	std::cout << "Copy constructor for ScavTrap called" << std::endl;
+	*this = to_copy; //assignment operator overload called
+}
+
+// Destructor
+ScavTrap::~ScavTrap () {
+
+	std::cout << "Destructor for ScavTrap called" << std::endl;
+}
+
+// Copy assignment Operator overload
+ScavTrap& ScavTrap::operator=(ScavTrap const & src) {
+
+	std::cout << "Copy assignment operator for ScavTrap called" << std::endl;
+
+	if (this != &src) {
+		this->_Name = src.getName();
+		this->_HitPoints = src.getHitPoints();
+		this->_EnergyPoints = src.getEnergyPoints();
+		this->_AttackDamage = src.getAttackDamage();
+	}
+	return *this;
+}
+
+/*
+*	Member function that performs an "attack" of _AttackDamage points to the opponent "target", and
+*	costs 1 Energy Point to the attacker (current ScavTrap object). If 0 Energy Points, no attack performed.
+*/
+void ScavTrap::attack(const std::string& target) {
+
+	if (this->_HitPoints == 0) {
+		std::cout << "ScavTrap " << this->_Name << " has been defeated and powered-off. No attack can be made." << std::endl;
+		return ;
+	}
+	if (this->_EnergyPoints >= 1) {
+		(this->_EnergyPoints)--;
+		std::cout << "ScavTrap " << this->_Name << BOLD(" attacks ") << target << ", causing " << this->_AttackDamage << " points of damage!" << std::endl;
+	}
+	else {
+		std::cout << "ScavTrap " << this->_Name << " doesn't have enough energy points to attack " << target << std::endl;
+	}
+}
+
+/*
+*	Additional Member Function that prints the current state of Hit Points, Energy Points
+*	and Attack Damage, of the the currentScavTrap object.
+*/
+void ScavTrap::printStatus() const {
+	
+	std::cout	<< "ScavTrap " << BOLD(this->_Name) << " currently has: "
+				<< this->_HitPoints << " Hit Points, " 
+				<< this->_EnergyPoints << " Energy Points, and "
+				<< this->_AttackDamage << " Attack Damage." << std::endl;
+}
+
---
+++ cpp03/ex01/main.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 21:53:02 by mevangel          #+#    #+#             */
+/*   Updated: 2024/04/26 04:21:29 by mevangel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main() {
+
+	/* Combination 1*/
+	ClapTrap A("Wall-e");
+	ScavTrap B("Terminator");
+	// /* Combination 2 */
+	// ClapTrap A("Wall-e", 20, 10, 5);
+	// ClapTrap B("Terminator", 15, 4, 3);
+	
+	// PART A: TESTING THE ATTACK
+	
+	unsigned int damageForAttack = 9;
+	std::cout << "\033[31m" << BOLD_UNDERLINE("\nTEST FOR THE ATTACK:") << "\033[0m" << std::endl;
+	
+	std::cout << UNDERLINE("\nState before the attack:") << std::endl;
+	A.printStatus();
+	B.printStatus();
+	
+	std::cout << UNDERLINE("\nThe attack:") << std::endl;
+	A.setAttackDamage(damageForAttack);
+	A.attack(B.getName());
+	B.takeDamage(A.getAttackDamage());
+
+	std::cout << UNDERLINE("\nState after the attack:") << std::endl;
+	A.printStatus();
+	B.printStatus();
+
+	// PART B: TESTING THE REPAIR
+	
+	unsigned int amountToRepair = 3;
+	std::cout << "\033[36m" << BOLD_UNDERLINE("\nTEST FOR THE REPAIR:") << "\033[0m" << std::endl;
+
+	std::cout << UNDERLINE("\nState before the repair:") << std::endl;
+	B.printStatus();
+
+	std::cout << UNDERLINE("\nThe repair:") << std::endl;
+	B.beRepaired(amountToRepair);
+	
+	std::cout << UNDERLINE("\nState after the repair:") << std::endl;
+	B.printStatus();
+	std::cout << std::endl;
+
+	return 0;
+}
