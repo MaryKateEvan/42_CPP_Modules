e29746ca31d8622d974bfaab95d850db251ff940
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-08-20 12:28:38 +0200

    fixed the main cause it was leaking before

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 04:00:20 by mevangel          #+#    #+#             */
-/*   Updated: 2024/08/20 12:02:51 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/08/20 12:25:59 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,35 +20,51 @@ int main() {
 	std::cout << CYAN(BOLD("          Testing the Performance of Interns      ")) << std::endl;
 	std::cout << CYAN("----------------------------------------------------------") << std::endl;
 
+	Intern someRandomIntern; //calls default constructor
+
+	AForm* form1 = NULL;
+	AForm* form2 = NULL;
+	AForm* form3 = NULL;
+	AForm* form4 = NULL;
+
 	try {
-		Intern someRandomIntern; //calls default constructor
-
-		// A. Valid Form Names:
-
 		std::cout << "\nAssigning our Intern to make a " << BOLD("\"shrubbery creation\"") << " form:" << std::endl;
-		AForm* form1 = someRandomIntern.makeForm("shrubbery creation", "plant");
+		form1 = someRandomIntern.makeForm("shrubbery creation", "plant");
 		std::cout << std::endl;
-
-		std::cout << "Assigning our Intern to make a " << BOLD("\"Robotomy Request\"") << " form:" << std::endl;
-		AForm* form2 = someRandomIntern.makeForm("Robotomy Request", "wall-e");
-		std::cout << std::endl;
-
-		std::cout << "Assigning our Intern to make a " << BOLD("\"PRESIDENTIAL PARDON\"") << " form:" << std::endl;
-		AForm* form3 = someRandomIntern.makeForm("PRESIDENTIAL PARDON", "Trump");
-		std::cout << std::endl;
-
-		// B. Invalid Form Names:
-		std::cout << "Assigning our Intern to make an " << BOLD("\"unknown form\"") << " :" << std::endl;
-		AForm* form4 = someRandomIntern.makeForm("unknown form", "some target");
-		
-		delete form1;
-		delete form2;
-		delete form3;
-		delete form4;
 	}
 	catch (const std::exception& e) {
-		std::cout << RED("❗ Exception found: " << UNDERLINE(e.what())) << std::endl << std::endl;
+		std::cout << RED("❗ Exception found while creating form1: " << UNDERLINE(e.what())) << std::endl << std::endl;
 	}
+
+	try {
+		std::cout << "Assigning our Intern to make a " << BOLD("\"Robotomy Request\"") << " form:" << std::endl;
+		form2 = someRandomIntern.makeForm("Robotomy Request", "wall-e");
+		std::cout << std::endl;
+	}
+	catch (const std::exception& e) {
+		std::cout << RED("❗ Exception found while creating form2: " << UNDERLINE(e.what())) << std::endl << std::endl;
+	}
+
+	try {
+		std::cout << "Assigning our Intern to make a " << BOLD("\"PRESIDENTIAL PARDON\"") << " form:" << std::endl;
+		form3 = someRandomIntern.makeForm("PRESIDENTIAL PARDON", "Trump");
+		std::cout << std::endl;
+	}
+	catch (const std::exception& e) {
+		std::cout << RED("❗ Exception found while creating form3: " << UNDERLINE(e.what())) << std::endl << std::endl;
+	}
+
+	try {
+		std::cout << "Assigning our Intern to make an " << BOLD("\"unknown form\"") << " :" << std::endl;
+		form4 = someRandomIntern.makeForm("unknown form", "some target");
+	} catch (const std::exception& e) {
+		std::cout << RED("❗ Exception found while creating form4: " << UNDERLINE(e.what())) << std::endl << std::endl;
+	}
+
+	delete form1;
+	delete form2;
+	delete form3;
+	delete form4;
 
 	return 0;
 }
