d5b5a06ecb1b5926ac8fc3f7df710c90f3d9c2ce
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-09-04 15:31:52 +0200

    shaped a bit better the tests in the main.cpp

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 16:06:52 by mevangel          #+#    #+#             */
-/*   Updated: 2024/09/04 15:19:09 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/09/04 15:31:00 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,61 @@
 
 static void run_tests() {
 	
-	std::cout << static_cast<int>('a') << std::endl;
+
+	// ScalarConverter::convert("a"); // Valid character
+	// ScalarConverter::convert("\\"); // Non-displayable character
+
+	// ScalarConverter::convert("34.12253");   // Invalid: empty string
+	// ScalarConverter::convert("45.235f");   // Invalid: empty string
+	// ScalarConverter::convert("0.42F");   // Invalid: empty string
+	// ScalarConverter::convert("0.12412");   // Invalid: empty string
+	// ScalarConverter::convert("%");   // Invalid: empty string
+
+	// ScalarConverter::convert("");   // Invalid: empty string
+	// ScalarConverter::convert("0.00f");   // Invalid: empty string
+	// ScalarConverter::convert("-2147483648");   // INT_MIN
+	// ScalarConverter::convert("2147483647");   // INT_MAX
+	// ScalarConverter::convert("-2147483649");   // smaller than INT_MIN
+	// ScalarConverter::convert("2147483649345678");   // smaller than INT_MIN
 	
-	ScalarConverter::convert("0"); // Valid character
-	ScalarConverter::convert("70"); // Valid character
+	std::cout << CYAN("\n--------------------------------------") << std::endl;
+	std::cout << CYAN(BOLD("     CATEGORY 1️⃣ : INPUT IS A CHAR    ")) << std::endl;
+	std::cout << CYAN("--------------------------------------") << std::endl;
+	ScalarConverter::convert("a");
+	ScalarConverter::convert(" ");
+	ScalarConverter::convert("*");
+	ScalarConverter::convert("\"");
+	ScalarConverter::convert("\\");
+	ScalarConverter::convert("}");
+
+	std::cout << CYAN("\n--------------------------------------") << std::endl;
+	std::cout << CYAN(BOLD("    CATEGORY 2️⃣ : INPUT IS INTEGER     ")) << std::endl;
+	std::cout << CYAN("--------------------------------------") << std::endl;
 	ScalarConverter::convert("42"); // Valid character
-	ScalarConverter::convert("a"); // Valid character
-	ScalarConverter::convert("\\"); // Non-displayable character
-	ScalarConverter::convert("hello"); // Invalid: more than one character
-	ScalarConverter::convert("");   // Invalid: empty string
-	ScalarConverter::convert("34.12253");   // Invalid: empty string
-	ScalarConverter::convert("45.235f");   // Invalid: empty string
-	ScalarConverter::convert("0.42F");   // Invalid: empty string
-	ScalarConverter::convert("0.12412");   // Invalid: empty string
-	ScalarConverter::convert("%");   // Invalid: empty string
-	ScalarConverter::convert("^35");   // Invalid: empty string
-	ScalarConverter::convert("..34");   // Invalid: empty string
-	ScalarConverter::convert("");   // Invalid: empty string
-	ScalarConverter::convert("0.00f");   // Invalid: empty string
-	ScalarConverter::convert("-2147483648");   // INT_MIN
-	ScalarConverter::convert("2147483647");   // INT_MAX
-	ScalarConverter::convert("-2147483649");   // smaller than INT_MIN
-	ScalarConverter::convert("2147483649345678");   // smaller than INT_MIN
-	
-	// std::cout << CYAN("\n--------------------------------------") << std::endl;
-	// std::cout << CYAN(BOLD("          TEST 1️⃣ : CHARS         ")) << std::endl;
-	// std::cout << CYAN("--------------------------------------") << std::endl;
+	ScalarConverter::convert("65"); // Valid character
+	ScalarConverter::convert("0"); // Non-displayable character
+	ScalarConverter::convert("10"); // Non-displayable character
+	ScalarConverter::convert("-2147483648"); // INT_MIN
+	ScalarConverter::convert("2147483647"); // INT_MAX
 
 	// std::cout << CYAN("\n--------------------------------------") << std::endl;
-	// std::cout << CYAN(BOLD("          TEST 2️⃣ : INTEGERS         ")) << std::endl;
+	// std::cout << CYAN(BOLD("     CATEGORY 3️⃣ : INPUT IS FLOAT    ")) << std::endl;
 	// std::cout << CYAN("--------------------------------------") << std::endl;
 	
 
 	// std::cout << CYAN("\n--------------------------------------") << std::endl;
-	// std::cout << CYAN(BOLD("          TEST 3️⃣ : FLOATS       ")) << std::endl;
+	// std::cout << CYAN(BOLD("     CATEGORY 4️⃣ : INPUT IS DOUBLE    ")) << std::endl;
 	// std::cout << CYAN("--------------------------------------") << std::endl;
 	
-
-	// std::cout << CYAN("\n--------------------------------------") << std::endl;
-	// std::cout << CYAN(BOLD("          TEST 4️⃣ : DOUBLES       ")) << std::endl;
-	// std::cout << CYAN("--------------------------------------") << std::endl;
+	std::cout << CYAN("\n--------------------------------------") << std::endl;
+	std::cout << CYAN(BOLD("    CATEGORY 5️⃣ : INPUT IS INVALID    ")) << std::endl;
+	std::cout << CYAN("--------------------------------------") << std::endl;
+	ScalarConverter::convert("hello");
+	ScalarConverter::convert("");
+	ScalarConverter::convert("^35");
+	ScalarConverter::convert("..34");
+	ScalarConverter::convert("-+21");
+	
 	
 }
 
