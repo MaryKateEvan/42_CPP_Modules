b40398a5a1528eb4bc6497f80da284cc8e450a9b
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-09-04 19:40:23 +0200

    first steps for ex02.

---
+++ cpp06/ex02/InheritingClasses.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   InheritingClasses.hpp                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 19:31:40 by mevangel          #+#    #+#             */
+/*   Updated: 2024/09/04 19:33:53 by mevangel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INHERITINGCLASSES_HPP
+# define INHERITINGCLASSES_HPP
+
+# include "Base.hpp"
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+#endif //INHERITINGCLASSES_HPP
