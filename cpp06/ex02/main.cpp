4b743bb6ceebd39ba8bbdb10cc9de764d4f076f7
Mary Kate Evangelidou <marykate_evan@outlook.com.gr>
2024-09-04 20:39:47 +0200

    added a very basic check for both the versions of the polymorphic identify function

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mevangel <mevangel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 19:52:19 by mevangel          #+#    #+#             */
-/*   Updated: 2024/09/04 20:24:16 by mevangel         ###   ########.fr       */
+/*   Updated: 2024/09/04 20:37:44 by mevangel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,6 @@
 // It randomly instanciates A, B or C class and returns the instance as a Base pointer.
 Base* generate(void) {
 	
-	srand(time(NULL)); // sets the seed for rand() using the current time (in seconds), to ensure the seed and randominsing is diferrent every time the program runs
-
 	int classID = rand() % 3;
 
 	if (classID == 0)
@@ -59,22 +57,54 @@ void identify(Base& p) {
 		std::cout << "This object is of class: " << BOLD("A") << std::endl;
 		return ;
 	}
-	catch (std::bad_cast &bc) {
+	catch (std::exception& e) {
 		//check for Class B:
 		try {
 			(void) dynamic_cast<B&>(p); //void casue i dont really need the cacsted value-reference. I just want to see if the cast is possible
 			std::cout << "This object is of class: " << BOLD("B") << std::endl;
 		}
-		catch (std::bad_cast &bc) {
+		catch (std::exception& e) {
 			//check for Class C:
 			try {
 				(void) dynamic_cast<C&>(p); //void casue i dont really need the cacsted value-reference. I just want to see if the cast is possible
 				std::cout << "This object is of class: " << BOLD("C") << std::endl;
 				return ;
 			}
-			catch (std::bad_cast& bc) {
+			catch (std::exception& e) {
 				std::cout << "This object does not belong to any of the A, B, C classes." << std::endl;
 			}
 		}
 	}
+}
+
+int main() {
+
+	srand(time(NULL)); // sets the seed for rand() using the current time (in seconds), to ensure the seed and randominsing is diferrent every time the program runs
+
+	// Random generation of 3 objects:
+	Base* ptr1 = generate();
+	Base* ptr2 = generate();
+	Base* ptr3 = generate();
+
+	Base& ref1 = *ptr1;
+	Base& ref2 = *ptr2;
+	Base& ref3 = *ptr3;
+
+	Base* invalid = NULL;
+
+	identify(ptr1);
+	identify(ptr2);
+	identify(ptr3);
+	std::cout << std::endl;
+	identify(ref1);
+	identify(ref2);
+	identify(ref3);
+	std::cout << std::endl;
+	identify(invalid);
+	
+	delete ptr1;
+	delete ptr2;
+	delete ptr3;
+
+	return 0;
 }
